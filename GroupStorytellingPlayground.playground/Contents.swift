625b55272cebe42864afd00a7200625c9f93ae67
cat8dog <catherine.reyto@gmail.com>
2015-08-10 17:13:29 -0400

    second storyline

--- GroupStorytellingPlayground.playground/Contents.swift
+++ GroupStorytellingPlayground.playground/Contents.swift
@@ -4,5 +4,5 @@ import UIKit
 
 var firstStorySentence = "In the ancient land of Eye Ohess there was a problem."
 
-var secondStorySentence = "The 
+var secondStorySentence = "The people of Ahn Dehrroid had been using their water and trampling through their forests." 
 
